585395fd36f2ae865fe467297c2f8e9d797c5ec8
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-09-30 18:15:13 -0700

    Fix linters errors

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -7,10 +7,10 @@ const Quote = () => (
       Mathematics is a language. — Josiah Willard Gibbs, American scientist
     </h3>
     <h3>
-    If you stop at general math, then you will only make general money. - Snoop Dogg
+      If you stop at general math, then you will only make general money. - Snoop Dogg
     </h3>
     <h3>
-    Pure mathematics is, in its way, the poetry of logical ideas. - Albert Einstein
+      Pure mathematics is, in its way, the poetry of logical ideas. - Albert Einstein
     </h3>
   </div>
 );
